5fce863164ba865ec09b8d94b348ebbeac84cd6c
metamn <bartus.csongor@gmail.com>
2016-03-13 15:08:20 +0200

    Ready to add the animations

--- code/project/intro/intro.js
+++ code/project/intro/intro.js
@@ -1,4 +1,19 @@
 var splitStringToChars = require('./../../framework/helpers/js/splitStringToChars.js');
+var klass = require('./../../framework/helpers/js/klass.js');
+var select = require('./../../framework/helpers/js/select.js');
+var click = require('./../../framework/helpers/js/click.js');
+
+var clickOnChars = function(charsID) {
+  chars = select(charsID);
+  click(chars, clickOnChar);
+
+  function clickOnChar(event) {
+    klass(document.body, 'show-intro', 'toggle');
+  }
+}
+
+
 if (document.querySelector('.intro__text')) {
   splitStringToChars('.intro__text', '.intro__chars');
+  clickOnChars('.intro__chars .char');
 }
