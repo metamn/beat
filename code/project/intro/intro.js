de68705633295ffe90913331425ba41b5e4511fd
metamn <bartus.csongor@gmail.com>
2016-03-13 13:24:03 +0200

    Typography for intro done

--- code/project/intro/intro.js
+++ code/project/intro/intro.js
@@ -1,2 +1,4 @@
 var splitStringToChars = require('./../../framework/helpers/js/splitStringToChars.js');
-splitStringToChars('.intro__text', '.intro__chars');
+if (document.querySelector('.intro__text')) {
+  splitStringToChars('.intro__text', '.intro__chars');
+}
