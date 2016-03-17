76a08a6f32be0ecb08620ca4420f8b5c536f95f0
metamn <bartus.csongor@gmail.com>
2016-03-17 15:50:25 +0200

    ALA artwork JS cleaned up

--- code/pages/what-some-people-apart-are-up-to-in-2016/__artwork/what-some-people-apart-are-up-to-in-2016__artwork.js
+++ code/pages/what-some-people-apart-are-up-to-in-2016/__artwork/what-some-people-apart-are-up-to-in-2016__artwork.js
@@ -10,21 +10,6 @@ var randomZindex = function(authorsID) {
   klass(authors[rnd], 'author--active', 'add')
 }
 
-var randomZindex2 = function(authorsID) {
-  var authors = document.querySelectorAll(authorsID);
-  var author = Math.floor((Math.random() * authors.length));
-
-  console.log('a:' + authors);
-
-  for (var i = 0; i < authors.length; i++) {
-    authors[i].style.zIndex = 0;
-
-    if (i == author) {
-      authors[i].style.zIndex = 1;
-    }
-  }
-}
-
 
 if (document.querySelector('.what-some-people-apart-are-up-to-in-2016__artwork .author')) {
   window.setInterval(function(){
