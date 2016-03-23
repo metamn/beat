38600112bf444913f932839f8476d60ae11e729e
metamn <bartus.csongor@gmail.com>
2016-03-23 18:35:27 +0200

    Contact artwork done on all pages

--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -1,3 +1,4 @@
 require('./../../project/article/--is-it-web-or-photoshop/article--is-it-web-or-photoshop.js');
 require('./../../project/article/--masters-of-web-february-2016/article--masters-of-web-february-2016.js');
 require('./../../project/article/__artwork/article__artwork.js');
+require('./../../project/contact-artwork/contact-artwork.js');
