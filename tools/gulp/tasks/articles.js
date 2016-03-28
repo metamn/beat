89f688e79c4a39b44333597cc75af5ce70828c3e
metamn <bartus.csongor@gmail.com>
2016-03-28 13:09:25 +0300

    Small fixes on the homepage layout

--- tools/gulp/tasks/articles.js
+++ tools/gulp/tasks/articles.js
@@ -44,6 +44,7 @@ gulp.task('articleCloseFile', function() {
 
 
 // Sort articles by date
+// - after working well for a year this code broke without being touched
 function sortByDate(data) {
   var sorted = [];
 
@@ -61,7 +62,12 @@ function sortByDate(data) {
 gulp.task('articleOrderArticles', function() {
   var articles = JSON.parse(fs.readFileSync(paths.articles_json, 'utf8'));
 
-  articles = sortByDate(articles);
+  //articles = sortByDate(articles);
+  articles.sort(function(a,b){
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
+    return new Date(b.date) - new Date(a.date);
+  });
 
   fs.openSync(paths.articles_json, 'w');
   fs.appendFileSync(paths.articles_json, JSON.stringify(articles));
