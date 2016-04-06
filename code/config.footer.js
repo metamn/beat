4ddc8c79de124cebaab4ec8d74c50990203bf8e2
metamn <bartus.csongor@gmail.com>
2016-04-07 00:33:00 +0300

    Intro and animation removed

--- code/config.footer.js
+++ code/config.footer.js
@@ -1,6 +1,5 @@
 // require here what goes into the footer
 require('./pages/home/home.js');
-require('./project/intro/intro.js');
 require('./pages/what-some-people-apart-are-up-to-in-2016/__ala/what-some-people-apart-are-up-to-in-2016__ala.js');
 require('./pages/ideally/ideally.js');
 require('./pages/tomorrowww/tomorrowww.js');
