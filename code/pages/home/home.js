3e251a0dc9dea71f257484c07ab22d46071174a4
metamn <bartus.csongor@gmail.com>
2016-07-31 20:08:23 +0300

    Working on Masters of Web July 2016

--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -2,3 +2,4 @@ require('./../../project/article/--is-it-web-or-photoshop/article--is-it-web-or-
 require('./../../project/article/--masters-of-web-february-2016/article--masters-of-web-february-2016.js');
 require('./../../project/article/__artwork/article__artwork.js');
 require('./../../project/contact-artwork/contact-artwork.js');
+require('./../../project/article/--masters-of-web-july-2016/article--masters-of-web-july-2016.js');
