6af851ca0d731887637d22555d0606be01e434c9
metamn <bartus.csongor@gmail.com>
2016-09-05 15:27:11 +0300

    Fixing slider

--- code/project/article/--masters-of-web-february-2016/article--masters-of-web-february-2016.js
+++
@@ -1,4 +0,0 @@
-var slider = require('./../../../framework/structure/slider/slider.js');
-if (document.querySelector('.article--masters-of-web-february-2016 .article__slider')) {
-  slider('.article--masters-of-web-february-2016 .article__slider', '.article--masters-of-web-february-2016 .article__bullets .bullet');
-}
