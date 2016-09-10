76bf0cee5e8edde8088b64746d84c7e17d12199f
metamn <bartus.csongor@gmail.com>
2016-09-10 11:38:35 +0300

    Working on Web trends

---
+++ code/pages/web-trends-september-2016/web-trends-september-2016.js
@@ -0,0 +1,4 @@
+var videoWithPoster = require('./../../framework/structure/video-with-poster/video-with-poster.js');
+if (document.querySelector('.video-with-poster')) {
+  videoWithPoster('.video-with-poster');
+}
