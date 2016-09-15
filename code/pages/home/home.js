2e200f4500180099e14c7c8e03db4a552cc27a9f
metamn <bartus.csongor@gmail.com>
2016-09-15 16:45:17 +0300

    Working on Web Trends artwork

--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -4,3 +4,4 @@ require('./../../project/article/__artwork/article__artwork.js');
 require('./../../project/contact-artwork/contact-artwork.js');
 require('./../../project/article/--masters-of-web-july-2016/article--masters-of-web-july-2016.js');
 require('./../../project/article/--web-trends-september-2016/article--web-trends-september-2016.js');
+require('./../web-trends-september-2016-artwork/web-trends-september-2016-artwork.js');
---
+++ code/pages/web-trends-september-2016-artwork/web-trends-september-2016-artwork.js
@@ -0,0 +1,1 @@
+require('./__horizontal-scrolling/web-trends-september-2016-artwork__horizontal-scrolling.js');
