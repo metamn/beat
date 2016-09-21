a6eb2474bd2df90e4975fbc81954ea184b83f409
metamn <bartus.csongor@gmail.com>
2016-09-21 14:10:09 +0300

    Rolling chars added

--- code/pages/web-trends-september-2016-artwork/web-trends-september-2016-artwork.js
+++ code/pages/web-trends-september-2016-artwork/web-trends-september-2016-artwork.js
@@ -1,2 +1,3 @@
 require('./__horizontal-scrolling/web-trends-september-2016-artwork__horizontal-scrolling.js');
 require('./__custom-spacers/web-trends-september-2016-artwork__custom-spacers.js');
+require('./__rolling-chars/web-trends-september-2016-artwork__rolling-chars.js');
