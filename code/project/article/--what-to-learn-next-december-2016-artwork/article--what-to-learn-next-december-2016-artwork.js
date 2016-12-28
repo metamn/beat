67c18a8361d04ce93eaee73c0de8757acb66bf92
metamn <bartus.csongor@gmail.com>
2016-12-28 17:24:27 +0200

    Working on the marquee

---
+++ code/project/article/--what-to-learn-next-december-2016-artwork/article--what-to-learn-next-december-2016-artwork.js
@@ -0,0 +1,5 @@
+var marquee3D = require('./../../../framework/design/decorations/marquee-3d/marquee-3d.js');
+
+if (document.querySelector('.article--what-to-learn-next-december-2016-artwork .marquee-3d')) {
+  marquee3D('.article--what-to-learn-next-december-2016-artwork .marquee-3d');
+}
