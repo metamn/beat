83714c96ef42e0a0a08d2f15ca94e8353e1edccf
metamn <bartus.csongor@gmail.com>
2016-12-29 11:50:58 +0200

    Cleanup the js

--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -5,4 +5,3 @@ require('./../../project/contact-artwork/contact-artwork.js');
 require('./../../project/article/--masters-of-web-july-2016/article--masters-of-web-july-2016.js');
 require('./../../project/article/--web-trends-september-2016/article--web-trends-september-2016.js');
 require('./../web-trends-september-2016-artwork/web-trends-september-2016-artwork.js');
-require('./../../project/article/--what-to-learn-next-december-2016-artwork/article--what-to-learn-next-december-2016-artwork.js');
--- code/project/article/--what-to-learn-next-december-2016-artwork/article--what-to-learn-next-december-2016-artwork.js
+++
@@ -1,5 +0,0 @@
-var marquee3D = require('./../../../framework/design/decorations/marquee-3d/marquee-3d.js');
-
-if (document.querySelector('.article--what-to-learn-next-december-2016-artwork .marquee-3d')) {
-  marquee3D('.article--what-to-learn-next-december-2016-artwork .marquee-3d');
-}
