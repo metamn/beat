12fa5a2621bca88f6c9d987814d49ec2aeab21bc
metamn <bartus.csongor@gmail.com>
2017-01-24 14:31:53 +0200

    Slider fixed

--- code/project/article/__slider/article__slider.js
+++ code/project/article/__slider/article__slider.js
@@ -1,5 +1,2 @@
 // Sliders are set up in the respective articles_json
 // like article-is-it-web-or-photoshop.js
-
-require('./../../article-is-it-web-or-photoshop/article-is-it-web-or-photoshop.js');
-require('./../../article-masters-of-web-february-2016/article-masters-of-web-february-2016.js');
