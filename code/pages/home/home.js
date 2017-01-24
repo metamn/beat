1a837c337bddc76fdfcfda456e46602276c94d71
metamn <bartus.csongor@gmail.com>
2017-01-24 10:25:10 +0200

    Working on brutalist websites

--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -5,3 +5,4 @@ require('./../../project/contact-artwork/contact-artwork.js');
 require('./../../project/article/--masters-of-web-july-2016/article--masters-of-web-july-2016.js');
 require('./../../project/article/--web-trends-september-2016/article--web-trends-september-2016.js');
 require('./../web-trends-september-2016-artwork/web-trends-september-2016-artwork.js');
+require('./../../project/article/--brutalist-websites-january-2017/article--brutalist-websites-january-2017.js');
