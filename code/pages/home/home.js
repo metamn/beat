b5f84aee3e03afa68cd4860fefa8306831664005
metamn <bartus.csongor@gmail.com>
2017-09-15 19:05:30 +0300

    Adding imagesLoading to all images

--- code/pages/home/home.js
+++ code/pages/home/home.js
@@ -1,3 +1,4 @@
+require('./../../project/articles/articles.js');
 require('./../../project/article/--is-it-web-or-photoshop/article--is-it-web-or-photoshop.js');
 require('./../../project/article/--masters-of-web-february-2016/article--masters-of-web-february-2016.js');
 require('./../../project/article/__artwork/article__artwork.js');
